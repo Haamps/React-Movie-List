27b399873b0f4b7cc9f7a21970094d7c22730d2b
Haamps <128600254+Haamps@users.noreply.github.com>
2024-05-12 23:43:26 +0200

    La till kommentarer

--- src/Stars.js
+++ src/Stars.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+//hämtar bilden på stjärnan och lägger den i en lista, kör därefter en for-loop som räknar stjärnorna baserat på filmens betyg
 export default function Stars({ rating }) {
     const starImage = 'star.png';
     const stars = [];
